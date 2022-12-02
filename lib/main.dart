16cecfc2635ea5231982acee253b46b00b70ff5a
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2022-12-02 14:43:25 +0100

    Change main color of app

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       },
       theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       home: const MySignupPage(title: "Inscription"),
     );
