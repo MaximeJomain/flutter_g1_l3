578aa64d3da8e90eab6d8ccfc4e44551df3edf6e
MaximeJomain <maxime.jomain95@gmail.com>
2022-12-02 14:40:54 +0100

    changed homepage

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const UserProfile(title: "Page de profil"),
+      home: const MySignupPage(title: "Inscription"),
     );
   }
 }
